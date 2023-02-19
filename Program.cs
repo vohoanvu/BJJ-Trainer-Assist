8ef7e280c15eb0e53d40cd7bfe710c14261e2057
Vu Vo <vohoanvu96@gmail.com>
2023-02-20 00:24:05 +0700

    mmdasd

--- Program.cs
+++ Program.cs
@@ -70,17 +70,18 @@ namespace BJJ_Trainer_Assist
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
-                app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-                app.UseDefaultFiles();
-                app.UseStaticFiles();
-            }
+            //else
+            //{
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    //app.UseHsts();
+
+            //}
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseHttpsRedirection();
 
