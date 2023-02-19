a5d60e76c75dfa8e9992169c9fd13b4466370467
Vu Vo <vohoanvu96@gmail.com>
2023-02-19 21:11:20 +0700

    fixed OIDC path

--- ClientApp/src/components/api-authorization/ApiAuthorizationConstants.js
+++ ClientApp/src/components/api-authorization/ApiAuthorizationConstants.js
@@ -23,7 +23,7 @@ const prefix = '/authentication';
 
 export const ApplicationPaths = {
   DefaultLoginRedirectPath: '/',
-  ApiAuthorizationClientConfigurationUrl: `_configuration/${ApplicationName}`,
+  ApiAuthorizationClientConfigurationUrl: `api/_configuration/${ApplicationName}`,
   ApiAuthorizationPrefix: prefix,
   Login: `${prefix}/${LoginActions.Login}`,
   LoginFailed: `${prefix}/${LoginActions.LoginFailed}`,
